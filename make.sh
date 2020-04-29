262c82e23d57d1c07d18ad41ca45dfe0952c76e6
Bhaskar Divya <bhaskar.divya007@gmail.com>
2020-04-29 19:19:45 +0530

    Fix packaging

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ rm -rf build
 mkdir build -p && cd build
 cmake -DCMAKE_INSTALL_PREFIX=`kf5-config --prefix` -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON ../
 make
-# sudo make package 
+sudo make package 
 sudo make install
 kquitapp5 plasmashell
 kstart5 plasmashell
